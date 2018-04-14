0b86fb9bc9abd4acbc7fe608ff737f1eddc34db4
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2018-04-15 00:50:14 +0400

    правки

--- html-element-collection/drum-machine/script.js
+++ html-element-collection/drum-machine/script.js
@@ -2,8 +2,10 @@
     const drumKits = document.getElementsByClassName('drum-kit__drum');
   
    for (let event of drumKits) {
-        event.onclick = function () {
+        event.onclick = function () { 
         const audioEvent = this.getElementsByTagName('audio')[0];
+        audioEvent.pause();
+        audioEvent.currentTime=0;
         audioEvent.play();
       }
     }
