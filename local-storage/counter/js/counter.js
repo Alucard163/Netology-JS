2c1ef589fa930299b564fb70a48b4cdef199b0c0
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2018-05-01 21:23:08 +0400

    v1

--- local-storage/counter/js/counter.js
+++ local-storage/counter/js/counter.js
@@ -1,1 +1,24 @@
 'use strict';
+const count = document.getElementById('counter');
+const inc = document.getElementById('increment');
+const dec = document.getElementById('decrement');
+const res = document.getElementById('reset');
+
+if (sessionStorage.countValue === undefined) {
+  sessionStorage.countValue = 0;
+}
+
+count.textContent = sessionStorage.countValue;
+
+inc.addEventListener('click', () => {
+  count.textContent = ++sessionStorage.countValue;
+});
+
+dec.addEventListener('click', () => {
+  count.textContent = sessionStorage.countValue <= 0 ? 0 : --sessionStorage.countValue;
+});
+
+res.addEventListener('click', () => {
+  sessionStorage.countValue = 0;
+  count.textContent = 0;
+});
--- local-storage/sign-in-form/js/auth.js
+++ local-storage/sign-in-form/js/auth.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const fieldSignIn = document.querySelector('.sign-in-htm');
+const fieldSignUp = document.querySelector('.sign-up-htm');
+const errSignIn = fieldSignIn.querySelector('.error-message');
+const errSignUp = fieldSignUp.querySelector('.error-message');
+
+initFormSubmit(fieldSignIn, 'https://neto-api.herokuapp.com/signin', errSignIn, 'signin');
+initFormSubmit(fieldSignUp, 'https://neto-api.herokuapp.com/signup', errSignUp, 'signup');
+
+function initFormSubmit(form, urlPOST, messageBlock, type) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const records = new FormData(event.currentTarget);
+    const convertToJson = {};
+
+    for (const [key, value] of records) {
+      convertToJson[key] = value;
+    }
+
+    const request = fetch(urlPOST, {
+      body: JSON.stringify(convertToJson),
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    });
+
+    request
+      .then((result) => {
+        if (200 <= result.status && result.status < 300) {
+          return result;
+        }
+        throw new Error(response.statusText);
+      })
+      .then((result) => result.json())
+      .then((data) => {
+        const messageOKText = type === 'signin' ? `Пользователь ${data.name} успешно авторизован` : `Пользователь ${data.name} успешно зарегистрирован`;
+        messageBlock.value = data.error ? data.message : messageOKText;
+      });
+
+  });
+}
